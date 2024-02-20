ccb6d9ea3e311240d3e481467da70be2f31fa9b5
Cedric De Boom <cedricdeboom@gmail.com>
2024-02-20 16:39:13 +0100

    Fix Dutch wordings and grammar in prompts

--- backend/src/llm.py
+++ backend/src/llm.py
@@ -36,14 +36,14 @@ def generate_prompt_for_decision_consult(topic_text:str, matching_decisions, max
 
     prompt_instruction = """
                 Het regeerakkoord bevat documenten die de plannen van de regering uitleggen.
-                De beslisssingen van de regering in de jaren nadien kunnen deze plannen uitvoeren, of van het originele akkoord afwijken.
-                Gegeven het regeringsakkoord en de beslissingen van de regering. 
-                Door alleen informatie uit de verstrekte documenten te gebruiken,
-                wordt een samenvatting op hoog niveau gegeven van hoe goed de regering
+                De beslissingen van de regering in de jaren nadien kunnen deze plannen uitvoeren, of van het originele akkoord afwijken.
+                Gegeven is het regeringsakkoord en de beslissingen van de regering. 
+                Enkel door gebruik te maken van informatie uit de verstrekte documenten,
+                geef een beknopte samenvatting van hoe goed de regering
                 haar beloften uit het regeerakkoord is nagekomen.
-                als een besluit geen relatie heeft met het regeerakkoord, beschouw het dan niet in de reactie.
+                Als een gegeven beslissing niets te maken heeft met het regeerakkoord, negeer deze beslissing dan.
                 Geef verwijzingen naar de beslissingen die in uw antwoord zijn gebruikt, en plaats de verwijzingen tussen vierkante haken, zoals [0][1][2] enz.
-                Geef aan het begin een titel voor uw antwoord en formatteer uw antwoord in markdown
+                Geef een titel voor uw antwoord en formatteer uw antwoord in markdown.
     """
 
     total_tokens = token_count(prompt_instruction) + token_count(topic_text)
@@ -64,7 +64,7 @@ def generate_prompt_for_decision_consult(topic_text:str, matching_decisions, max
                 Regeerakkoord: {topic_text} 
                 Beslissingen: {flat_decisions}
 
-                Jou antwoord:
+                Uw antwoord:
     """
     return prompt, included_decision_count
 
@@ -88,12 +88,12 @@ def decisions_query(query:str, decisions:list[BedrockRetrievedItem])->str:
     flat_decisions = "\n".join(flat_decisions)
     prompt = f"""
                 Beantwoord de gegeven vraag uitsluitend met behulp van de informatie in het onderstaande gedeelte over regeringsbeslissingen.
-                reageer op een gemoedelijke manier, niet met een lijst
-                geef het indexnummer van de gebruikte referentie op tussen enkelvoudige vierkante haken, zoals [1].
+                Reageer op een gemoedelijke manier, niet met een lijst.
+                Geef het indexnummer van de gebruikte referentie tussen enkelvoudige vierkante haken, zoals [1].
 
-                regeringsbeslissingen: {flat_decisions}
-                vraag:{query}
-                Jou antwoord:
+                Regeringsbeslissingen: {flat_decisions}
+                Vraag:{query}
+                Jouw antwoord:
     """
     return get_response(prompt)
     
